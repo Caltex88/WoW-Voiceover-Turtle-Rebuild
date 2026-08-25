5f2447d0a5960b262c7b18b3738249cb8ff7ffe3
Caltex88 <KBWray@outlook.com>
2026-08-25 14:06:45 -0500

    Delete Version.lua

--- Version.lua
+++
@@ -1,8 +0,0 @@
-setfenv(1, VoiceOver)
-Version = {}
-
-local CLIENT_VERSION, BUILD, _, INTERFACE_VERSION = GetBuildInfo()
-
-Version.Client    = CLIENT_VERSION
-Version.Build     = BUILD
-Version.Interface = INTERFACE_VERSION or 0
